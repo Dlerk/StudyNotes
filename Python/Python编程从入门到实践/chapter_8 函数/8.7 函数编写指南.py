6bcff0920c066ae25feae35e3b5b63d7dfb7618c
Dlerk_ <103750116+Dlerk@users.noreply.github.com>
2022-05-03 17:33:58 +0800

    python函数_22.5.53_17:33

---
+++ Python/Python编程从入门到实践/chapter_8 函数/8.7 函数编写指南.py
@@ -0,0 +1,13 @@
+# 应给函数指定描述性名称，且只在其中使用小写字母和下划线
+
+# 每个函数都应包含简要地阐述其功能的注释
+
+# 给形参指定默认值时，等号两边不要有空格
+# 对于函数调用中的关键字实参，也应遵循这种约定
+
+def function_name(parameter_0, parameter_1='default value')
+
+function_name(value_0, parameter_1='value')
+
+# 所有import 语句都应放在文件开头。
+# 唯一例外的情形是，在文件开头使用了注释来描述整个程序。
