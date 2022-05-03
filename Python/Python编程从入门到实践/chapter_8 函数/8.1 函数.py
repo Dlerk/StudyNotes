214bb9e8ebd74f0996f9f97bf7d09931d1088ad1
Dlerk_ <103750116+Dlerk@users.noreply.github.com>
2022-05-03 10:53:53 +0800

    01背包的两种算法_22.5.3_10:53

---
+++ Python/Python编程从入门到实践/chapter_8 函数/8.1 函数.py
@@ -0,0 +1,26 @@
+# 定义函数
+
+def greet_user():
+    """显示简单的问候语。"""
+    print("Hello!")
+
+greet_user()
+
+
+# 传参
+
+def greet_user(username):
+    """显示简单的问候语。"""
+    print(f"Hello, {username.title()}!")
+
+greet_user('jesse')
+
+# 关键字实参
+# 
+
+
+
+
+
+
+
